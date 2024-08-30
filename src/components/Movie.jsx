55641be8da41607f385ec8c4041f5b58079d662a
Luis Angel Arroyo Morales <36710715+luanarmo@users.noreply.github.com>
2024-08-30 16:02:56 -0600

    feat: implementa alto y ancho en imagen

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,7 +2,7 @@
 export default function Movie({ movie }) {
     return (
         <li key={movie.id} className='movie'>
-            <img src={movie.poster} alt={movie.title} />
+            <img src={movie.poster} alt={movie.title} style={{ width: '200px', height: '300px' }} />
             <h2>{movie.title}</h2>
             <p>{movie.year}</p>
         </li>
